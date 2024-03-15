8953dcea92a9e996486095572f6448f5ee62fe47
ahmedkhan90255 <ahmed.m.khan123@gmail.com>
2024-03-15 08:44:42 -0700

    2 questions

---
+++ Question no.1/question1.js
@@ -0,0 +1,18 @@
+// var matrix = [
+//     [1, 2, 3]
+//     [4, 5, 6]
+//     [7, 8, 9]
+// ]
+var arr1 = [
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3]
+]
+
+// document.write (matrix)
+
+for (let column of arr1) {
+    // var arr1join = row.join()
+    console.log (arr1)
+}
+
