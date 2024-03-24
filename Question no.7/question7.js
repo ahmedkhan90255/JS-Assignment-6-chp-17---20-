1a11a2b84e6bbe12d2a8c35bc11fa379cbcc3685
ahmedkhan90255 <ahmed.m.khan123@gmail.com>
2024-03-23 23:21:46 -0700

    All questions complete javascript assignments chp 17 -20

---
+++ Question no.7/question7.js
@@ -0,0 +1,42 @@
+document.write(
+  "<h1> You have an array <br> A = [“cake”, “apple pie”, “cookie”, “chips”, “patties”] <br> Write a program to enable “search by user input” in an array.<br> After searching, prompt the user whether the given item is <br> found in the list or not. Example:</h1>"
+);
+
+var foodItems = ["cake", "apple pie", "cookie", "chips", "patties"];
+var userInput = prompt("Enter the food item you want");
+
+if (
+  userInput === "cake" ||
+  userInput === "apple pie" ||
+  userInput === "cookie" ||
+  userInput === "chips" ||
+  userInput === "patties"
+) {
+  alert(userInput + " is available");
+}
+// } else {
+//   alert(userInput + " is not available");
+// }
+
+if (
+  userInput === "Cake" ||
+  userInput === "Apple pie" ||
+  userInput === "Cookie" ||
+  userInput === "Chips" ||
+  userInput === "Patties"
+) {
+  alert(userInput + " is available");
+} else if (
+  userInput !== "cake" ||
+  userInput === "apple pie" ||
+  userInput === "cookie" ||
+  userInput === "chips" ||
+  userInput === "patties" ||
+  userInput === "Cake" ||
+  userInput === "Apple pie" ||
+  userInput === "Cookie" ||
+  userInput === "Chips" ||
+  userInput === "Patties"
+) {
+  alert(userInput + " is not available");
+}
